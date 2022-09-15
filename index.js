ca9491753d483e2f38b6aa966c5b66d272198df3
Emily Wu <99505073+emily825@users.noreply.github.com>
2022-09-15 03:54:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const d = new Date();
-let m = d.getMonth();
-let h = d.getHours();
-
+let month = d.getMonth();
+let hour = d.getHours();
 let bb = document.querySelector('body');
 let hb = document.querySelector('h1');
 let fb = document.querySelector('footer');
 
-if (m == 0 || m ==1) {
-	if (h >= 7 && h < 17) {
+let day = d.getDay();
+let quote = document.querySelector('#quote');
+let list = ["Trust yourself and think big!", "Hope you have a great week!", "You can do it!", "Dreams don't work unless you do!", "Wear your heart on your sleeve!", "Hope you have a great weekend!", "Recharging for productivity!"];
+
+if (month == 0 || month ==1) {
+	if (hour >= 7 && hour < 17) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -16,8 +19,8 @@ if (m == 0 || m ==1) {
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
 	}
-} else if (m == 2 || m == 3) {
-	if (h >= 6 && h < 19) {
+} else if (month == 2 || month == 3) {
+	if (hour >= 6 && hour < 19) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -26,8 +29,8 @@ if (m == 0 || m ==1) {
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
 	}
-} else if (m == 4 || m == 5 || m == 6) {
-	if (h >= 5 && h < 20) {
+} else if (month == 4 || month == 5 || month == 6) {
+	if (hour >= 5 && hour < 20) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -36,8 +39,8 @@ if (m == 0 || m ==1) {
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
 	}
-} else if (m == 7 || m == 8) {
-	if (h >= 6 && h < 19) {
+} else if (month == 7 || month == 8) {
+	if (hour >= 6 && hour < 19) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -46,8 +49,8 @@ if (m == 0 || m ==1) {
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
 	}
-} else if (m == 9) {
-	if (h >= 7 && h < 18) {
+} else if (month == 9) {
+	if (hour >= 7 && hour < 18) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -56,8 +59,8 @@ if (m == 0 || m ==1) {
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
 	}
-} else if (m == 10 || m == 11) {
-	if (h >= 7 && h < 16) {
+} else if (month == 10 || month == 11) {
+	if (hour >= 7 && hour < 16) {
 		bb.style.backgroundColor = '#fff5f9';
 		hb.style.setProperty('background', 'lightcoral');
 		fb.style.setProperty('background', 'lightcoral');
@@ -65,5 +68,43 @@ if (m == 0 || m ==1) {
 		bb.style.backgroundColor = '#f5f5ff';
 		hb.style.setProperty('background', '#8080f0');
 		fb.style.setProperty('background', '#8080f0');
+	}
+}
+
+if (day == 0) {
+	quote.innerHTML = list[day];
+} else if (day == 1) {
+	quote.innerHTML = list[day];
+} else if (day == 2) {
+	quote.innerHTML = list[day];
+} else if (day == 3) {
+	quote.innerHTML = list[day];
+} else if (day == 4) {
+	quote.innerHTML = list[day];
+} else if (day == 5) {
+	quote.innerHTML = list[day];
+} else if (day == 6) {
+	quote.innerHTML = list[day];
+}
+
+function change() {
+	day = day + 1
+	if (day >= 7) {
+		day = 1
+	}
+	if (day == 0) {
+		quote.innerHTML = list[day];
+	} else if (day == 1) {
+		quote.innerHTML = list[day];
+	} else if (day == 2) {
+		quote.innerHTML = list[day];
+	} else if (day == 3) {
+		quote.innerHTML = list[day];
+	} else if (day == 4) {
+		quote.innerHTML = list[day];
+	} else if (day == 5) {
+		quote.innerHTML = list[day];
+	} else if (day == 6) {
+		quote.innerHTML = list[day];
 	}
 }
